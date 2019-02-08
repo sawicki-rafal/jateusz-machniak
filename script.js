56b8477f569eac2fd6c6eb40be1c7d7deefa05f7
sawraf <sawicki-rafal>
2019-02-08 22:25:09 +0100

    refactor + group conversation photos change

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 function changeContent() {
 
-    function changePhotos() {
-        let photos = document.getElementsByClassName("img");
 
-        for (let i = 0; i < photos.length; i++) {
-            photos[i].src = "https://z-p3-scontent-waw1-1.xx.fbcdn.net/v/t1.15752-9/46170045_723043544736253_8450186384936796160_n.jpg?_nc_cat=106&_nc_ht=z-p3-scontent-waw1-1.xx&oh=880f1071415abe1985f0091e09b9a6ad&oe=5CF364C6";
-        }
+    function changePhotos() {
+        const JATEUSZ_PHOTO_URL = "https://z-p3-scontent-waw1-1.xx.fbcdn.net/v/t1.15752-9/46170045_723043544736253_8450186384936796160_n.jpg?_nc_cat=106&_nc_ht=z-p3-scontent-waw1-1.xx&oh=880f1071415abe1985f0091e09b9a6ad&oe=5CF364C6";
+        let personalConversationPhotos = Array.prototype.filter.call(document.getElementsByClassName("img"), photo => photo.className === "img");
+        let groupConversationPhotoDivs = Array.prototype.filter.call(document.querySelectorAll('[style*=background-image]'), photo => photo.className === "");
+
+        Array.prototype.forEach.call(personalConversationPhotos, photo => photo.src = JATEUSZ_PHOTO_URL);
+        Array.prototype.forEach.call(groupConversationPhotoDivs, photoDiv => {
+            photoDiv.style.backgroundImage = 'url("' + JATEUSZ_PHOTO_URL + '")';
+        });
     }
 
 
     function changeConversationsNames() {
 
-        function changeConversationNamesFor(ariaLabelTag){
+        function changeConversationNamesFor(ariaLabelTag) {
+            const JATEUSZ_NAME = "Jateusz Machniak";
             let spans = document.querySelectorAll('[aria-label="' + ariaLabelTag + '"] span');
-            if (spans) {
-                for (let i = 0; i < spans.length; i++) {
-                    spans[i].innerHTML = "Jateusz Machniak";
-                }
-            }
+            Array.prototype.forEach.call(spans,span => span.innerHTML = JATEUSZ_NAME);
         }
 
         changeConversationNamesFor("Lista konwersacji");
@@ -27,7 +28,7 @@ function changeContent() {
 
     setInterval(function () {
         changePhotos();
-        changeConversationsNames()
+        changeConversationsNames();
     }, 1000);
 }
 
